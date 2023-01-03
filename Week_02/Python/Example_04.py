2fa33fc5fc3a4e06d0cb5e4e38fb8887e9a16e81
imseongjoo <afro1662@naver.com>
2023-01-03 14:35:05 +0900

    230103_04

---
+++ Week_02/Python/Example_04.py
@@ -0,0 +1,72 @@
+# a = 1
+# b = 1
+# print(a < b) # False
+
+
+# a = bool("")
+# b = False
+# print(a == b) # True
+
+
+# a = 1
+# result = ""
+
+# if a == 1:
+#     result = True
+# else:
+#     result = False
+
+# print(result) # True
+
+
+# a = 90 
+
+# if a == 90:
+#     a = a + 10
+
+# elif a == 100:
+#     a = a + 10
+
+# elif a == 110:
+#     a = a + 10
+
+# else:
+#     a = a + 10
+
+# a = a + 10
+
+# print(a) # 110
+
+
+# string = "hello world!"
+
+# for element in string:
+#     print(element)
+    
+# """
+# 예측을 작성하세요.
+# ?
+# """
+# h
+# e
+# l
+# l
+# o
+
+# w
+# o
+# r
+# l
+# d
+# !
+
+
+list_variable = [0, 1, 2, 3, 4, 5, 6]
+
+for element in list_variable:
+    print(element, end=" ")
+    
+"""
+예측을 작성하세요.
+?
+"""
