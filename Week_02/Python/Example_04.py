9ead546c3b32d83168b1c2f5eab1c8bd27a2ec09
imseongjoo <afro1662@naver.com>
2023-01-03 17:56:13 +0900

    230103_07

--- Week_02/Python/Example_04.py
+++ Week_02/Python/Example_04.py
@@ -1,167 +1,167 @@
-# a = 1
-# b = 1
-# print(a < b) # False
+a = 1
+b = 1
+print(a < b) # False
 
 
-# a = bool("")
-# b = False
-# print(a == b) # True
+a = bool("")
+b = False
+print(a == b) # True
 
 
-# a = 1
-# result = ""
+a = 1
+result = ""
 
-# if a == 1:
-#     result = True
-# else:
-#     result = False
+if a == 1:
+    result = True
+else:
+    result = False
 
-# print(result) # True
-
-
-# a = 90 
-
-# if a == 90:
-#     a = a + 10
-
-# elif a == 100:
-#     a = a + 10
-
-# elif a == 110:
-#     a = a + 10
-
-# else:
-#     a = a + 10
-
-# a = a + 10
-
-# print(a) # 110
+print(result) # True
 
 
-# string = "hello world!"
+a = 90 
 
-# for element in string:
-#     print(element)
+if a == 90:
+    a = a + 10
+
+elif a == 100:
+    a = a + 10
+
+elif a == 110:
+    a = a + 10
+
+else:
+    a = a + 10
+
+a = a + 10
+
+print(a) # 110
+
+
+string = "hello world!"
+
+for element in string:
+    print(element)
     
-# """
-# 예측을 작성하세요.
-# ?
-# """
-# h
-# e
-# l
-# l
-# o
+"""
+예측을 작성하세요.
+?
+"""
+h
+e
+l
+l
+o
 
-# w
-# o
-# r
-# l
-# d
-# !
+w
+o
+r
+l
+d
+!
 
-# list_variable = [0, 1, 2, 3, 4, 5, 6]
+list_variable = [0, 1, 2, 3, 4, 5, 6]
 
-# for element in list_variable:
-#     print(element, end=" ")
+for element in list_variable:
+    print(element, end=" ")
     
-# """
-# 예측을 작성하세요.
-# ?
-# """
-# 0 1 2 3 4 5 6
+"""
+예측을 작성하세요.
+?
+"""
+0 1 2 3 4 5 6
 
 
-# n = 10
+n = 10
 
-# for element in range(-n, n):
-#     print(element, end=" ")
+for element in range(-n, n):
+    print(element, end=" ")
     
-# """
-# 예측을 작성하세요.
-# ?
-# """
-# -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 
+"""
+예측을 작성하세요.
+?
+"""
+-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 
 
 
-# n = 10
+n = 10
 
-# for element in range(1, n + 1, 3):
-#     print(element, end=" ")
+for element in range(1, n + 1, 3):
+    print(element, end=" ")
     
-# """
-# 예측을 작성하세요.
-# ?
-# """
-# 1 4 7 10
+"""
+예측을 작성하세요.
+?
+"""
+1 4 7 10
 
 
-# list_variable = [6, 5, 4, 3, 2, 1, 0] 
+list_variable = [6, 5, 4, 3, 2, 1, 0] 
 
-# # enumerate가 무엇인지 검색해보세요!
-# for index, element in enumerate(list_variable):
-#     print(index, element)
+# enumerate가 무엇인지 검색해보세요!
+for index, element in enumerate(list_variable):
+    print(index, element)
     
-# """
-# 예측을 작성하세요.
-# ?
-# """
-# 0 6
-# 1 5
-# 2 4
-# 3 3
-# 4 2
-# 5 1
-# 6 0
+"""
+예측을 작성하세요.
+?
+"""
+0 6
+1 5
+2 4
+3 3
+4 2
+5 1
+6 0
 
 
-# n = 10
+n = 10
 
-# for element in range(n, -n, -1):
-#     print(element, end=" ")
-#     if n < 0:
-#         break
-# """
-# 예측을 작성하세요.
-# ?
-# """
-# 10 9 8 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6 -7 -8 -9 
+for element in range(n, -n, -1):
+    print(element, end=" ")
+    if n < 0:
+        break
+"""
+예측을 작성하세요.
+?
+"""
+10 9 8 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6 -7 -8 -9 
 
 
-# list_variable = [-1, 3, 5, -2, 1, 9, 21, -3, -5]
+list_variable = [-1, 3, 5, -2, 1, 9, 21, -3, -5]
 
-# for element in list_variable:
-#     if element < 0:
-#         continue
+for element in list_variable:
+    if element < 0:
+        continue
     
-#     print(element, end=" ")
+    print(element, end=" ")
     
-# """
-# 예측을 작성하세요.
-# ?
-# """
-# 3 5 1 9 21 
+"""
+예측을 작성하세요.
+?
+"""
+3 5 1 9 21 
 
 
-# N = 3
-# M = 4
+N = 3
+M = 4
 
-# for n in range(N):
-#     for m in range(M):
-#         print(f"{n}, {m}")
-# """
-# 예측을 작성하세요.
-# ?
-# """
-# 0, 0
-# 0, 1
-# 0, 2
-# 0, 3
-# 1, 0
-# 1, 1
-# 1, 2
-# 1, 3
-# 2, 0
-# 2, 1
-# 2, 2
-# 2, 3
+for n in range(N):
+    for m in range(M):
+        print(f"{n}, {m}")
+"""
+예측을 작성하세요.
+?
+"""
+0, 0
+0, 1
+0, 2
+0, 3
+1, 0
+1, 1
+1, 2
+1, 3
+2, 0
+2, 1
+2, 2
+2, 3
--- Week_02/Python/Example_05.py
+++ Week_02/Python/Example_05.py
@@ -1,72 +1,72 @@
-# num1 = int(input('정수를 입력하세요 > '))
-# num2 = 0
-# if num1 > 0:
-#     print('True')
-# else:
-#     print('False')
+num1 = int(input('정수를 입력하세요 > '))
+num2 = 0
+if num1 > 0:
+    print('True')
+else:
+    print('False')
 
-# num1 = int(input('첫 번째 정수를 입력하세요 > '))
-# num2 = int(input('두 번째 정수를 입력하세요 > '))
-# if num1 > num2:
-#     print(num1)
-# elif num1 < num2:
-#     print(num2)
-# else:
-#     print('False')
+num1 = int(input('첫 번째 정수를 입력하세요 > '))
+num2 = int(input('두 번째 정수를 입력하세요 > '))
+if num1 > num2:
+    print(num1)
+elif num1 < num2:
+    print(num2)
+else:
+    print('False')
 
-# num1 = int(input('정수를 입력하세요 > '))
-# num2 = 1
-# num3 = 10
-# if num3 > num1 > num2:
-#     print('True')
-# else:
-#     print('False')
+num1 = int(input('정수를 입력하세요 > '))
+num2 = 1
+num3 = 10
+if num3 > num1 > num2:
+    print('True')
+else:
+    print('False')
 
-# num1 = int(input('정수를 입력하세요 > '))
-# if num1%2 == 0 and num1 > num2:
-#     print('True')
-# else:
-#     print('False')
+num1 = int(input('정수를 입력하세요 > '))
+if num1%2 == 0 and num1 > num2:
+    print('True')
+else:
+    print('False')
 
-# num1 = int(input('정수를 입력하세요 > '))
-# if 100 < num1 or num1 < 0:
-#     print('에러')
-# elif num1 >= 60:
-#     print('합격')
-# elif num1 < 60:
-#     print('불합격')
+num1 = int(input('정수를 입력하세요 > '))
+if 100 < num1 or num1 < 0:
+    print('에러')
+elif num1 >= 60:
+    print('합격')
+elif num1 < 60:
+    print('불합격')
 
-# letter = input('문자열을 입력하세요 >  ')
-# print(letter[::-1])
+letter = input('문자열을 입력하세요 >  ')
+print(letter[::-1])
 
-# num1 = int(input('첫 번째 정수를 입력하세요 > '))
-# num2 = int(input('두 번째 정수를 입력하세요 > '))
-# arr = (i for i in range(min(num1, num2)+1,max(num1, num2)))
-# if arr:
-#     print(*arr, sep='\n')
-# elif num1 == num2:
-#     print('False')
+num1 = int(input('첫 번째 정수를 입력하세요 > '))
+num2 = int(input('두 번째 정수를 입력하세요 > '))
+arr = (i for i in range(min(num1, num2)+1,max(num1, num2)))
+if arr:
+    print(*arr, sep='\n')
+elif num1 == num2:
+    print('False')
 
-# num1 = int(input('첫 번째 정수를 입력하세요 > '))
-# num2 = int(input('두 번째 정수를 입력하세요 > '))
-# if num1 == num2:
-#     print('False')
-# elif num1 < num2:
-#     for A in reversed(range(num1 + 1, num2)):
-#         print(A, end = " ")
-# else:
-#     for B in reversed(range(num2 + 1, num1)):
-#         print(B, end = " ")
+num1 = int(input('첫 번째 정수를 입력하세요 > '))
+num2 = int(input('두 번째 정수를 입력하세요 > '))
+if num1 == num2:
+    print('False')
+elif num1 < num2:
+    for A in reversed(range(num1 + 1, num2)):
+        print(A, end = " ")
+else:
+    for B in reversed(range(num2 + 1, num1)):
+        print(B, end = " ")
 
-# num1 = int(input('정수를 입력하세요 > '))
-# num2 = 1
-# if num1 < num2:
-#     print('False')
-# elif num1 >= num2:
-#     for i in range(num2,num1):
-#         if i % 2 ==1: 
-#           print(i)
+num1 = int(input('정수를 입력하세요 > '))
+num2 = 1
+if num1 < num2:
+    print('False')
+elif num1 >= num2:
+    for i in range(num2,num1):
+        if i % 2 ==1: 
+          print(i)
 
-# for x in range(2, 10):
-#     for y in range(1, 10):
-#         print(x, "X", y, "=", x*y)
+for x in range(2, 10):
+    for y in range(1, 10):
+        print(x, "X", y, "=", x*y)
