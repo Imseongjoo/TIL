7539f0b81e8f71b5ff07748e833bf41b93e4f344
imseongjoo <afro1662@naver.com>
2023-01-03 15:11:29 +0900

    230103_05

--- Week_02/Python/Example_04.py
+++ Week_02/Python/Example_04.py
@@ -60,13 +60,107 @@
 # d
 # !
 
+# list_variable = [0, 1, 2, 3, 4, 5, 6]
 
-list_variable = [0, 1, 2, 3, 4, 5, 6]
-
-for element in list_variable:
-    print(element, end=" ")
+# for element in list_variable:
+#     print(element, end=" ")
     
-"""
-예측을 작성하세요.
-?
-"""
+# """
+# 예측을 작성하세요.
+# ?
+# """
+# 0 1 2 3 4 5 6
+
+
+# n = 10
+
+# for element in range(-n, n):
+#     print(element, end=" ")
+    
+# """
+# 예측을 작성하세요.
+# ?
+# """
+# -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 
+
+
+# n = 10
+
+# for element in range(1, n + 1, 3):
+#     print(element, end=" ")
+    
+# """
+# 예측을 작성하세요.
+# ?
+# """
+# 1 4 7 10
+
+
+# list_variable = [6, 5, 4, 3, 2, 1, 0] 
+
+# # enumerate가 무엇인지 검색해보세요!
+# for index, element in enumerate(list_variable):
+#     print(index, element)
+    
+# """
+# 예측을 작성하세요.
+# ?
+# """
+# 0 6
+# 1 5
+# 2 4
+# 3 3
+# 4 2
+# 5 1
+# 6 0
+
+
+# n = 10
+
+# for element in range(n, -n, -1):
+#     print(element, end=" ")
+#     if n < 0:
+#         break
+# """
+# 예측을 작성하세요.
+# ?
+# """
+# 10 9 8 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6 -7 -8 -9 
+
+
+# list_variable = [-1, 3, 5, -2, 1, 9, 21, -3, -5]
+
+# for element in list_variable:
+#     if element < 0:
+#         continue
+    
+#     print(element, end=" ")
+    
+# """
+# 예측을 작성하세요.
+# ?
+# """
+# 3 5 1 9 21 0, 0
+
+
+# N = 3
+# M = 4
+
+# for n in range(N):
+#     for m in range(M):
+#         print(f"{n}, {m}")
+# """
+# 예측을 작성하세요.
+# ?
+# """
+# 0, 1
+# 0, 2
+# 0, 3
+# 1, 0
+# 1, 1
+# 1, 2
+# 1, 3
+# 2, 0
+# 2, 1
+# 2, 2
+# 2, 3
