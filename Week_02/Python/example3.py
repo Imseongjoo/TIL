f9ef3ad995074fa9a28395e3f2fe3839e0b5dd89
imseongjoo <afro1662@naver.com>
2023-01-03 10:53:11 +0900

    230101

--- Week_02/Python/example3.py
+++
@@ -1,7 +0,0 @@
-# 홀수/짝수
-
-num = int(input(입력))
-if num%2 == 1:
-    print('홀수')
-else: 
-    print('짝수')
---
+++ Week_02/Python/example_03.py
@@ -0,0 +1,20 @@
+# 홀수/짝수
+
+num = int(input('입력'))
+if num%2 == 1:
+    print('홀수')
+else: 
+    print('짝수')
+
+# 미세먼지
+
+dust = 80
+if dust  > 150:
+    print('매우 나쁨')
+elif dust > 80:
+    print('나쁨')
+elif dust > 30:
+    print('보통')
+elif
+    print('좋음')
+print('미세먼지 확인 완료')
