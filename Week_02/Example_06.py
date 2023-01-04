641e20f82755e42d7ddaa08c464461c959a1d199
imseongjoo <afro1662@naver.com>
2023-01-04 10:03:12 +0900

    230104_03

---
+++ Week_02/Example_06.py
@@ -0,0 +1,11 @@
+target_number = 1-
+n = 0
+result = 0
+조건 : target_number >= n
+반복 때마다 n+=1, result 더하기!
+
+while target_number >= n:
+    result += n
+    n += 1
+    print(result, n)
+print(result)
