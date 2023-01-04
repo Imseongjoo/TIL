77113291e8a5a5dedf663b116c550aca80a86bb6
imseongjoo <afro1662@naver.com>
2023-01-04 14:45:16 +0900

    230104_07

--- Week_02/Example_08_예제.py
+++ Week_02/Example_08_예제.py
@@ -144,5 +144,5 @@
 # list_variable = [0, 1, 2, 3, 4, 5, 6]
 # print(len(list_variable)) # 7
 
-list_variable = [0, 1, 2, 3, 4, 5, 6]
-print(sum(list_variable)) # ?
+# list_variable = [0, 1, 2, 3, 4, 5, 6]
+# print(sum(list_variable)) # 21
