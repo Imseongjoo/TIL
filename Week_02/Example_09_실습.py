e4f41c55466287b29ca40189cd14ea055686ea26
imseongjoo <afro1662@naver.com>
2023-01-05 09:21:16 +0900

    230105_01

--- Week_02/Example_09_실습.py
+++ Week_02/Example_09_실습.py
@@ -8,6 +8,12 @@ if num >= 0:
 else:
     print(num*-1)
 
+# number = int(input("정수를 입력하세요 > "))
+# if number > 0:
+#     print(number)
+# else:
+#     print(number * -1)
+
 # 문제 2
 # 정수만 저장한 리스트가 주어집니다.
 # 리스트 요소의 개수를 출력하세요.
@@ -18,6 +24,14 @@ cnt = 0
 for number in number_list:
     cnt += 1
 print(cnt)
+
+# number_list = [1, 2, 3, 4, 5]
+# count = 0
+
+# for number in number_list:
+#     count = count + 1
+
+# print(count)
 
 number_list = [0]
 cnt = 0
@@ -35,6 +49,14 @@ result = 0
 for number in number_list:
     result += int(number)
 print(result)
+
+# number_list = [1, 2, 3, 4, 5]
+# total = 0
+
+# for number in number_list:
+#     total = total + number
+
+# print(total)
 
 number_list = [-1, -2, -3, -4, -5]
 result = 0
@@ -54,6 +76,16 @@ for number in number_list:
     result += int(number)
     cnt += 1
 print(int(result/cnt))
+
+number_list = [2, 4, 6]
+
+# total = 0
+# count = 0
+# for number in number_list:
+#     total = total + number
+#     count = count + 1
+
+# print(total / count)
 
 number_list = [2, 3, 5, 7]
 result = 0
@@ -79,6 +111,12 @@ for number in number_list:
     result *= int(number)
 print(result)
 
+# number_list = [-1, -2, 3]
+# prod = 1
+# for number in number_list:
+#     prod = prod * number
+# print(prod)
+
 # 문제 6
 # 양의 정수만 저장한 리스트가 주어집니다.
 # 리스트에 저장된 정수 중 가장 큰 값을 출력하세요.
@@ -90,6 +128,13 @@ for i in number_list:
     if i > big:
         big = i
 print(big)
+
+# number_list = [1, 2, 5, 4, 3]
+# max_value = 0
+# for number in number_list:
+#     if number > max_value:
+#         max_value = number
+# print(max_value)
 
 number_list = [1, 1, 1]
 big = number_list[0]
@@ -116,3 +161,10 @@ for i in number_list:
     if i < small:
         small = i
 print(small)
+
+# number_list = [5, 2, 5, 5]
+# min_value = number_list[0]
+# for number in number_list:
+#     if number < min_value:
+#         min_value = number
+# print(min_value)
