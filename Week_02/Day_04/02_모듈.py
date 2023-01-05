19c22c78c1fe6ad2f67eb990d5aad6f8f16c8f14
imseongjoo <afro1662@naver.com>
2023-01-05 17:53:19 +0900

    230105_05

--- Week_02/Day_04/02_모듈.py
+++ Week_02/Day_04/02_모듈.py
@@ -1,27 +1,39 @@
 # 1. 모듈을 가져오는 것!
 import random
 
-menu = ['햄버거', '국밥','초밥']
+menu = ['햄버거', '국밥', '초밥']
 print(random.choice(menu))
 
 # 로또 추첨 코드 작성
 # random.sample(population, k)
-# Return a k length list 6개 숫자
-# the popuation sequence 1~45개 숫자 중 : range(1, 46)
+# Return a k length list   6개 숫자
+# the population sequence. 1~45개 숫자 중 : range(1, 46)
 
 for i in range(5):
     numbers = range(1, 46)
     lucky_numbers = random.sample(numbers, 6)
     print(sorted(lucky_numbers))
 
-print(sorted(random.sample(range(1, 46), 6)))
-
-students =  ['민욱', '홍엽', '현석', '정은']
+students = ['민욱', '홍엽', '현석', '정은']
 print(students)
 random.shuffle(students)
 print(students)
 
-# .sort() : 매서드
+import datetime
+
+# print(datetime.datetime.now())
+# print(datetime.date(2023, 1, 5))
+
+today = datetime.datetime.today()
+# print(today)
+# print(type(today))
+# print(today.year)
+# print(today.day)
+
+end = datetime.datetime(2023, 6, 14)
+print(f'우리가 개발자가 되는 시간... {end - today}')
+
+# .sort() : 메서드
 # return : None
 # 해당 리스트 자체를 정렬!
 numbers = [10, 2, 5]
@@ -33,26 +45,12 @@ numbers = [10, 2, 5]
 numbers.sort()
 print(numbers)
 
-# .sorted.() : 함수
+# sorted() : 함수
 # return : 정렬된 리스트
 numbers = [10, 2, 5]
 result = sorted(numbers)
 print(result) # [2, 5, 10]
 
-import datetime
-
-print(datetime.datetime.now())
-print(datetime.date(2023, 1, 5))
-
-today = datetime.date(2023, 1,5)
-print(today)
-print(type(today))
-print(today, year)
-print(today.day)
-
-end = datetime.date(2023, 6, 14)
-print(f'우리가 개발자가 되는 시간... {end - today}')
-
-import os
+import os 
 
 print(os.listdir())
---
+++ Week_02/Day_04/04_실습.py
@@ -0,0 +1,114 @@
+# 문제 1
+# 문자열을 입력 받고 문자열에서 e 의 개수를 구해서 출력하세요
+# 단, count() 함수는 사용하지마세요
+
+str = input("문자열을 입력하세요 > ")
+list = list(str)
+cnt = 0
+for x in list:
+    if "e" in x:
+        cnt += 1
+    else:
+        cnt += 0
+print(cnt)
+
+# 문제 2
+# 문자열을 입력 받고, 문자열 중 알파벳 모음의 총 개수를 출력하세요
+# 알파벳 모음 : a(A) e(E) i(I) o(O) u(U)
+# 단, count() 함수는 사용하지마세요
+
+str = input("문자열을 입력하세요 > ")
+list = list(str)
+cnt = 0
+for x in list:
+    if "a" in x:
+        cnt += 1
+    elif "A" in x:
+        cnt += 1
+    elif "e" in x:
+        cnt += 1
+    elif "E" in x:
+        cnt += 1
+    elif "i" in x:
+        cnt += 1
+    elif "I" in x:
+        cnt += 1
+    elif "o" in x:
+        cnt += 1
+    elif "O" in x:
+        cnt += 1  
+    elif "u" in x:
+        cnt += 1
+    elif "U" in x:
+        cnt += 1 
+    else:
+        cnt += 0
+print(cnt)
+
+# 문제 3
+# 입력과 같은 딕셔너리 변수가 있을 때, 
+# 해당 인물의 나이를 출력하세요
+
+# 입력
+dict_variable = {
+    "이름": "정우영",
+    "생년": "2000",
+    "회사": "하이퍼그로스",
+}
+
+### 이하 문제 해결 코드 작성
+dict_variable["나이"] = 30
+print(f'나이 : {dict_variable["나이"]}세')
+
+# 문제 4
+# 이름, 전화번호, 거주지 정보를 입력받아 딕셔너리 구조로 저장하고,
+# 해당 딕셔너리와 딕셔너리의 정보를 구분해서 출력하세요
+
+dict_variable = {
+    "이름": input("이름을 입력하세요 > "),
+    "전화번호": input("전화번호를 입력하세요 >"),
+    "거주지": input("거주지를 입력하세요 > "),
+}
+for key in dict_variable:
+    print(f"{key} : {dict_variable[key]}")
+
+# 문제 5
+# 이름, 전화번호, 이메일, 거주지 정보를 입력받아 
+# 출력 예시와 동일한 딕셔너리 구조를 출력하세요
+# Hint : 딕셔너리 안에 딕셔너리를 넣을 수 있습니다
+
+name = input("이름을 입력하세요 > ")
+number = input("전화번호를 입력하세요 > ")
+email = input("이메일을 입력하세요 > ")
+live = input("거주지를 입력하세요 > ")
+
+dict_variable = {
+    name :
+    {"전화번호": number,
+    "이메일": email,
+    "거주지": live}
+}   
+print(dict_variable)
+
+# 출력 예시
+# 이름을 입력하세요 > 정우영
+# 전화번호를 입력하세요 > 010-1234-5678
+# 이메일을 입력하세요 > beemo@hphk.kr 
+# 거주지를 입력하세요 > 서울시 
+# {'정우영': {'전화번호': '010-1234-5678', '이메일': 'beemo@hphk.kr', '거주지': '서울시'}}
+
+# 문제 6
+# 문자열을 입력받고, 문자열에서 개별 문자가 나오는 횟수를 출력하세요
+
+# 출력 예시 1
+# 문자열을 입력하세요 > hello =
+# h 1
+# e 1
+# l 2
+# o 1
+
+# 출력 예시 2
+# 문자열을 입력하세요 > else =
+# e 2
+# l 1
+# s 1
