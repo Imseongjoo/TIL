e998276c143f9471d553aa5263645f4956b7bd82
imseongjoo <afro1662@naver.com>
2023-01-06 17:55:17 +0900

    230106_06

--- Week_02/Day_05/PJT-01/05.py
+++ Week_02/Day_05/PJT-01/05.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 
 # 아래 코드 수정 금지
 movie_json = open("data/movie.json", encoding="UTF8")
@@ -10,3 +11,19 @@ movie_info_dict = {}
 for key in key_list:
     movie_info_dict[key] = movie[key]
 print(movie_info_dict)
+
+# import json
+
+# # 아래 코드 수정 금지
+# movie_json = open('data/movie.json', encoding='UTF8')
+# movie = json.load(movie_json)
+
+# # 이하 문제 해결을 위한 코드 작성
+# new_movie = {
+#     'id': movie['id'],
+#     'title': movie['title'],
+#     'vote_average': movie['vote_average'],
+#     'overview': movie['overview'],
+#     'genre_ids': movie['genre_ids'],
+# }
+# print(new_movie)
--- Week_02/Day_05/PJT-01/06.py
+++ Week_02/Day_05/PJT-01/06.py
@@ -11,3 +11,27 @@ for key in key_list:
     movies[key] = movies_list[key]
     print(movies)
 
+# import json
+
+# # 아래 코드 수정 금지
+# movies_json = open('data/movies.json', encoding='UTF8')
+# movies_list = json.load(movies_json)
+
+# genres_json = open('data/genres.json', encoding='UTF8')
+# genres_list = json.load(genres_json)
+
+# # 이하 문제 해결을 위한 코드 작성
+# new_movie_list = []
+
+# for movie in movies_list:
+#     new_movie = {
+#         'id': movie['id'],
+#         'title': movie['title'],
+#         'poster_path': movie['poster_path'],
+#         'vote_average': movie['vote_average'],
+#         'overview': movie['overview'],
+#         'genre_ids': movie['genre_ids'],
+#     }
+#     new_movie_list.append(new_movie)
+
+# print(new_movie_list)
