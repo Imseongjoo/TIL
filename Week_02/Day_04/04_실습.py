6419ea8eb29c6180ed94c477db31dc3b9d3fa130
imseongjoo <afro1662@naver.com>
2023-01-06 09:21:24 +0900

    230106_01

--- Week_02/Day_04/04_실습.py
+++ Week_02/Day_04/04_실습.py
@@ -12,6 +12,14 @@ for x in list:
         cnt += 0
 print(cnt)
 
+# string = input("문자열을 입력하세요 > ")
+# count = 0
+# for char in string:
+#     if char == "e":
+#         count += 1
+
+# print(count)
+
 # 문제 2
 # 문자열을 입력 받고, 문자열 중 알파벳 모음의 총 개수를 출력하세요
 # 알파벳 모음 : a(A) e(E) i(I) o(O) u(U)
@@ -45,6 +53,25 @@ for x in list:
         cnt += 0
 print(cnt)
 
+# string = input("문자열을 입력하세요 > ")
+# count = 0
+# for char in string:
+#     if (
+#         char == "e"
+#         or char == "E"
+#         or char == "a"
+#         or char == "A"
+#         or char == "i"
+#         or char == "I"
+#         or char == "o"
+#         or char == "O"
+#         or char == "u"
+#         or char == "U"
+#     ):
+#         count += 1
+
+# print(count)
+
 # 문제 3
 # 입력과 같은 딕셔너리 변수가 있을 때, 
 # 해당 인물의 나이를 출력하세요
@@ -60,6 +87,16 @@ dict_variable = {
 dict_variable["나이"] = 30
 print(f'나이 : {dict_variable["나이"]}세')
 
+# dict_variable = {
+#     "이름": "정우영",
+#     "생년": "2000",
+#     "회사": "하이퍼그로스",
+# }
+
+# dict_variable["나이"] = 2023 - int(dict_variable["생년"]) + 1
+
+# print(dict_variable["나이"])
+
 # 문제 4
 # 이름, 전화번호, 거주지 정보를 입력받아 딕셔너리 구조로 저장하고,
 # 해당 딕셔너리와 딕셔너리의 정보를 구분해서 출력하세요
@@ -71,6 +108,18 @@ dict_variable = {
 }
 for key in dict_variable:
     print(f"{key} : {dict_variable[key]}")
+
+# name = input("이름을 입력하세요 > ")
+# phone_number = input("전화번호를 입력하세요 > ")
+# residence = input("거주지를 입력하세요 > ")
+# dict_variable = {
+#     "이름": name,
+#     "전화번호": phone_number,
+#     "거주지": residence,
+# }
+# print(dict_variable)
+# for key, value in dict_variable.items():
+#     print(f"{key} : {value}")
 
 # 문제 5
 # 이름, 전화번호, 이메일, 거주지 정보를 입력받아 
@@ -90,6 +139,20 @@ dict_variable = {
 }   
 print(dict_variable)
 
+# name = input("이름을 입력하세요 > ")
+# phone_number = input("전화번호를 입력하세요 > ")
+# email = input("이메일을 입력하세요 > ")
+# residence = input("거주지를 입력하세요 > ")
+
+# dict_variable = {
+#     name: {
+#         "전화번호": phone_number,
+#         "이메일": email,
+#         "거주지": residence,
+#     }
+# }
+# print(dict_variable)
+
 # 출력 예시
 # 이름을 입력하세요 > 정우영
 # 전화번호를 입력하세요 > 010-1234-5678
@@ -101,14 +164,27 @@ print(dict_variable)
 # 문자열을 입력받고, 문자열에서 개별 문자가 나오는 횟수를 출력하세요
 
 # 출력 예시 1
-# 문자열을 입력하세요 > hello =
+# 문자열을 입력하세요 > hello 
 # h 1
 # e 1
 # l 2
 # o 1
 
-# 출력 예시 2
-# 문자열을 입력하세요 > else =
-# e 2
-# l 1
-# s 1
+str = input("문자열을 입력하세요 > ")
+dic = {}
+char = []
+for char in str:
+        try: dic[char] += 1
+        except: dic[char] = 1
+print(dic)
+
+# string = input("문자열을 입력하세요 > ")
+# dict_variable = {}
+# for char in string:
+#     if char in dict_variable.keys():
+#         dict_variable[char] += 1
+#     elif char not in dict_variable.keys():
+#         dict_variable[char] = 1
+
+# for key, value in dict_variable.items():
+#     print(key, value)
