105da832febc31d891f605b705b02b007d1eef32
imseongjoo <afro1662@naver.com>
2023-01-10 12:16:16 +0900

    230110_03

--- Week_03/Day_01/03_실습02.py
+++ Week_03/Day_01/03_실습02.py
@@ -7,18 +7,33 @@
 # 문자열을 입력하세요 > hello # 사용자 입력
 # 1
 
-# str = input('문자열을 입력하세요 > ')
-# i = ''
-# cnt = 0
-# for i in str:
-#     if 'e' not in str:
-#             print(int(-1))
-#             break
-#     elif 'e' not in i:
-#             cnt += 1     
-#     elif 'e' in i:
-#             print(int(cnt))
-       
+str = input('문자열을 입력하세요 > ')
+i = ''
+cnt = 0
+for i in str:
+    if 'e' not in str:
+            print(int(-1))
+            break
+    elif 'e' not in i:
+            cnt += 1     
+    elif 'e' in i:
+            print(int(cnt))
+
+# string = input("문자열을 입력하세요 > ")
+# for index in range(len(string)):
+#     if string[index] == 'e':
+#         print(index)
+#         break
+# else:
+#     print(-1)
+
+# string = input("문자열을 입력하세요 > ")
+# result = -1
+# for index in range(len(string)):
+#     if string[index] == 'e':
+#         result = index
+# print(result)
+ 
 # 문제 2
 # 문자열을 입력받고, 각 단어의 등장 횟수를 출력하세요.
 # 단, count() 메서드는 사용하지마세요.
@@ -27,8 +42,21 @@
 # 문자열을 입력하세요 > hello # 사용자 입력
 # hello 1
 
-# str = input('문자열을 입력하세요 > ').split()
-# print(len(str))
+str = input('문자열을 입력하세요 > ').split()
+print(len(str))
+
+# string = input("문자열을 입력하세요 > ").split()
+# dict_var = {}
+
+# for word in string:
+#     if word in dict_var:
+#         dict_var[word] += 1
+
+#     elif word not in dict_var:
+#         dict_var[word] = 1
+
+# for key, value in dict_var.items():
+#     print(f"{key} {value}")
 
 # 문제 3
 # 문자열을 입력받고, e 를 제거한 결과를 출력하세요.
@@ -38,10 +66,17 @@
 # 문자열을 입력하세요 > apple # 사용자 입력
 # appl
 
-# x = input("문자열을 입력하세요 > ")
-# num = x.index('e')
-# strx = x[:num] + x[num+1:]
-# print(strx)
+x = input("문자열을 입력하세요 > ")
+num = x.index('e')
+strx = x[:num] + x[num+1:]
+print(strx)
+
+# string = input("문자열을 입력하세요 > ")
+# new_string = ""
+# for char in string:
+#     if char != 'e':
+#         new_string += char
+# print(new_string)
 
 # 문제 4
 # 문자열과 알파벳을 공백으로 구분해서 입력받고, 
@@ -52,15 +87,23 @@
 # 문자열을 입력하세요 > apple p # 사용자 입력
 # 2
 
-# a, b = input('문자열 알파벳').split()
-# i = ''
-# cnt = 0
-# for i in a:
-#     if b in i:
-#             cnt += 1     
-#     elif b in i:
-#             cnt = 0
-# print(int(cnt))
+a, b = input('문자열 알파벳').split()
+i = ''
+cnt = 0
+for i in a:
+    if b in i:
+            cnt += 1     
+    elif b in i:
+            cnt = 0
+print(int(cnt))
+
+# string, alpha = input("문자열을 입력하세요 > ").split()
+# count = 0
+# for char in string:
+#     if char == alpha:
+#         count += 1
+
+# print(count)
 
 # 문제 5
 # 양의 정수 3개를 입력받고, 3개의 양수를 - 로 연결해서 출력하세요.
@@ -70,11 +113,19 @@
 # 문자열을 입력하세요 > 010 1234 5678 # 사용자 입력
 # 010-1234-5678
 
-# a = int(input('자연수를 입력하세요 > '))
-# b = int(input('자연수를 입력하세요 > '))
-# c = int(input('자연수를 입력하세요 > '))
-# k = str(a)+'-'+str(b)+'-'+str(c)
-# print(k)
+a = int(input('자연수를 입력하세요 > '))
+b = int(input('자연수를 입력하세요 > '))
+c = int(input('자연수를 입력하세요 > '))
+k = str(a)+'-'+str(b)+'-'+str(c)
+print(k)
+
+# string_list = input("문자열을 입력하세요 > ").split()
+
+# #### 방법 1
+# print(string_list[0], string_list[1], string_list[2], sep='-')
+
+# #### 방법 2
+# print(*string_list, sep="-")
 
 # 문제 6
 # 양의 정수를 입력받고, 자리수의 합을 출력하세요.
@@ -86,10 +137,36 @@
 # 10
 
 n =  int(input())
-result =0
 
-while n > 0:
-    result += n%10
-    n //= 20
+if n < 0:
+    print(-1)
+else:
+    result =0
+    while n > 0:
+        result += n%10
+        n //= 10
+    print(result)
 
-print(result)
+# # while 사용 
+# # 반복적으로 n을 10으로 나눈 몫, 
+# # n이 0보다 클 때 계속 반복!
+# # 결과값은 n을 10으로 나눈 나머지를 더해나갈 것이다!
+
+# n = int(input())
+
+# if n < 0: 
+#     print(-1)
+# else:
+#     result = 0
+#     while n > 0:
+#         result += n%10
+#         n //= 10
+#     print(result)
+
+# n = input()
+
+# result = 0
+# for i in n:
+#     result += int(i)
+
+# print(result)
---
+++ Week_03/Day_02/01_함수.py
@@ -0,0 +1,106 @@
+def add(a, b): # 정의
+    return a + b 
+
+print(add(-10, -20)) # 호출
+print(add(10, 20))
+
+# 하나의 숫자를 입력 받아서 세제곱을 반환하는 
+# 함수 cube를 작성
+def cube(number):
+    return number**3
+
+# 함수 호출 
+# 2의 세제곱
+print(cube(2))
+# 100의 세제곱
+print(cube(100))
+
+n = 43 + 55
+print(n)
+
+def foo():
+    return 1, 2, 3
+
+print(foo())
+print(type(foo()))
+
+def boo():
+    a = 1 + 2
+
+print(boo())
+
+# return
+# 명시적인 return문 없는 경우 : None
+# 여러 값을 return 하는 경우 : tuple
+
+a = divmod(4, 2)
+print(a)
+
+print('홍엽', '유영', '윤원', '진아')
+
+def add(*numbers):
+    # print(type(numbers)) # tuple
+    result = 0
+    for n in numbers: 
+        result += n
+    return result
+
+print(add(1, 2, 3, 10, 23))
+
+def movie(**kwargs):
+    return kwargs
+
+print(movie(name='더 글로리', writer='김은숙'))
+
+# open(file, mode='r', buffering=- 1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
+# open('README.md', 'r', encoding='UTF8')
+# open('README.md', mode='r', encoding='UTF8')
+# open('README.md', 'r', 'UTF8') # 다르게 동작! 왜? buffering이 세번째!
+
+print(sum)
+
+# global scope
+a = 10 
+
+# local scope
+def foo():
+    b = 10 
+
+foo()
+print(b) # NameError!
+
+# sum : Built-in scope
+print(sum([1, 2, 3]))
+
+# sum : Global scope
+sum = 1 + 2
+
+print(type(sum), sum)
+print(sum([1, 2, 3]))
+
+a = 5
+
+def foo():
+    print(a) # Local scope에 a가 없네?
+
+foo()
+
+a = 5
+
+def boo():
+    global a
+    a = 3
+    print(a) 
+
+boo()
+print(a)
+# a = 5 + 6
+# boo()
+# a = foo()
+
+for i in range(3):
+    print(i)
+
+print(i) # 2 존재
+
+# for, if는 별도의 scope를 가지지 않습니다.
