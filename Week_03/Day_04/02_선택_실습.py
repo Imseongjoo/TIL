46d23d709278f20cc73ca358f9ebdeb1b465e7ac
Seongjoo Lim <afro1662@naver.com>
2023-01-12 14:05:42 +0900

    230112_03

---
+++ Week_03/Day_04/02_선택_실습.py
@@ -0,0 +1,89 @@
+# 명세서
+
+# 클래스 이름
+# Calculator
+
+# 인스턴스 변수
+# number1 : 정수 타입
+# number2 : 정수 타입
+
+# 생성자
+# 두 개의 정수를 인자로 받아서 number1, number2 에 저장합니다.
+
+# 메소드
+# plus
+# number1 + number2를 반환(return)합니다.
+
+# minus
+# number1 - number2를 반환(return)합니다.
+
+# multiply
+# number1 * number2를 반환(return)합니다.
+
+# division
+# number1 // number2를 반환(return)합니다.
+
+# print
+# 인스턴스 변수와 모든 사칙연산 결과를 출력(print)합니다.
+
+# 출력 예시
+
+# plus
+### 실행 코드
+# calculator = Calculator(10, 5)
+# print(calculator.plus())
+
+# calculator.number1 = -2
+# calculator.number2 = 2
+# print(calculator.plus())
+
+### 출력
+# 15
+# 0
+
+# minus
+### 실행 코드
+# calculator = Calculator(10, 5)
+# print(calculator.minus())
+
+# calculator.number1 = -2
+# calculator.number2 = 2
+# print(calculator.minus())
+
+### 출력
+# 5
+# -4
+
+# multiply
+### 실행 코드
+# calculator = Calculator(10, 5)
+# print(calculator.multiply())
+
+# calculator.number1 = -2
+# calculator.number2 = 2
+# print(calculator.multiply())
+
+### 출력
+# 50
+# -4
+
+# division
+### 실행 코드
+# calculator = Calculator(10, 5)
+# print(calculator.division())
+
+# calculator.number1 = -2
+# calculator.number2 = 2
+# print(calculator.division())
+
+### 출력
+# number1 = 10, number2 = 5
+# 합 : 15
+# 빼기 : 5
+# 곱 : 50
+# 몫 : 2
+# number1 = -2, number2 = 2
+# 합 : 0
+# 빼기 : -4
+# 곱 : -4
+# 몫 : -1
