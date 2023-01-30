403da612e5ad888e073be591e5e4a47f183beb02
Seongjoo Lim <afro1662@naver.com>
2023-01-30 12:03:59 +0900

    230130_01

---
+++ Week_06/Day_01/01_Last_Week.py
@@ -0,0 +1,77 @@
+import sys
+
+# heapq 모듈 import
+from heapq import heapify,heappop,heappush
+
+sys.stdin = open("input.txt")
+
+# input() 보다 빠른 입력 방법
+N = int(sys.stdin.readline().strip())
+
+# 연산의 개수
+# print(N)
+
+# 연산을 저장할 리스트
+number_list = []
+
+for _ in range(N):
+    number = int(sys.stdin.readline().strip())
+
+    # 입력받은 연산
+    # print(number)
+
+    # heappop
+    if number == 0:
+        # heap이 비어있을때
+        if len(number_list) == 0:
+            pass
+            
+        # heap이 비어있지 않을 때
+        elif len(number_list) != 0:
+            pass
+        
+
+    # heappush
+    elif number != 0:
+        # (절대값, 원본값) push
+        heappush(number_list, (abs(number), number))
+        # print(number,number_list)
+
+import sys
+
+sys.stdin = open("input.txt")
+
+N = int(input())
+
+# 단어의 수
+# print(N)
+
+# 딕셔너리
+# 키 : 단어 / 밸류 : 단어의 길이
+word_len = dict()
+
+# 단어의 수만큼 반복문
+for _ in range(N):
+    word = input()
+    
+    # 입력받은 단어
+    # print(word)
+    word_len[word] = len(word)
+
+
+# 단어와 단어의 길이를 저장한 딕셔너리
+# print(word_len)
+
+# 정렬은 어떤 함수를 사용해야하나?
+# sorted(값, 정렬 기준)
+
+# 딕셔너리를 정렬할 때 출력해야하는 값
+# print(word_len.items())
+
+# sorted 정렬 기준이 2개 이상일 때 -> 튜플 형태
+sorted_word_len = sorted(word_len.items(), key = lambda x:(x[1],x[0]))
+# print(sorted_word_len)
+
+# 정렬한 값을 순회하며 출력
+for value in sorted_word_len:
+    print(value[0])
---
+++ Week_06/Day_01/02_이차원_리스트.py
@@ -0,0 +1,79 @@
+N = 4
+M = 3
+
+matrix = [[0] * M] * N 
+print(matrix)
+matrix[0][0] = 1
+print(matrix)
+
+print('====================')
+
+matrix2 = [[0] * M for _ in range(N)]
+print(matrix2)
+matrix2[0][0] = 1
+print(matrix2)
+
+
+# 리스트 복사
+a = [1, 2, 3]
+b = list(a) # a[:]
+a[0] = 100
+
+# deepcopy
+
+import pprint
+# 1. 손수 만들기
+# matrix = [
+#     [0, 0, 0],
+#     [0, 0, 0],
+#     [0, 0, 0]
+# ]
+
+# 2. for문 활용 
+# print([0] * 10)
+# 4X3 행렬 만든다고 하면
+N = 4 # 행
+M = 3 # 열
+
+matrix = []
+for _ in range(N):
+    # [0, 0, 0, ... 0] 
+    matrix.append([0] * M)
+
+pprint.pprint(matrix)
+
+# 3. 리스트 컴프리헨션
+# 반복하면서 append 특정 값을 할 때
+# 어떠한 요소로 구성된 리스트 만들 때 
+
+matrix = [[0] * M for _ in range(N)]
+pprint.pprint(matrix)
+
+# 1. 8 * 8
+# text = '.F.F...F'
+# # 어떻게 바꾼다?
+# # ['.', 'F', '.', ..., 'F']
+# list(text)
+
+import pprint
+
+# 1-1. 반복문
+matrix = []
+for _ in range(8):
+    # text = '.F.F...F'
+    matrix.append(list(input()))
+
+pprint.pprint(matrix)
+
+# 1-2. list comprehension
+# line = '.F.F...F'
+matrix = [list(input()) for _ in range(8)]
+
+# 2. 3X3
+# line = '1 2 3'
+# [1, 2, 3]
+matrix = [ list(map(int, input().split())) for _ in range(3)]
+
+# matrix = []
+# for _ in range(3):
+#     matrix.append(__________________)
