97a5e287950f8e026b66694289260cad5e226fd8
Seongjoo Lim <afro1662@naver.com>
2023-03-30 00:20:23 +0900

    230330_01

--- 2023_03/Week_04/Day_03/01_Django_ORM/articles/urls.py
+++ 2023_03/Week_04/Day_03/01_Django_ORM/articles/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pk>/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
-    path('create/', views.create, name='create')
+    path('create/', views.create, name='create'),
 ]
 
 
--- 2023_03/Week_04/Day_03/01_Django_ORM/articles/views.py
+++ 2023_03/Week_04/Day_03/01_Django_ORM/articles/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from .models import Article
 
+
 # Create your views here.
-
-
 def index(request):
-    # DB에 전체 게시글 조회를 요청
+    # DB에 전체 게시글 조회를 요청하고 쿼리셋을 응답받아 저장
     articles = Article.objects.all()
     # print(articles)
     context = {
@@ -32,7 +31,7 @@ def create(request):
     # print(request.GET)
     title = request.GET.get('title')
     content = request.GET.get('content')
-
+    
     # 받은 데이터를 DB에 저장
     # 1
     # article = Article()
@@ -41,12 +40,12 @@ def create(request):
     # article.save()
 
     # 2
+    # 저장 전에 유효성 검사와 같은 추가 작업을 위해 2번 방법을 택함
     article = Article(title=title, content=content)
-    # 저장 전에 유효성 검사
     article.save()
 
     # 3
-    Article.objects.create(title=title, content=content)
+    # Article.objects.create(title=title, content=content)
 
     # 결과 페이지 반환
     return render(request, 'articles/create.html')
